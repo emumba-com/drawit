ed33c3a2453bef7602c0bd09ce108ad0fb74dc96
umarashfaq <umar.mughal2@gmail.com>
2017-10-30 23:10:16 +0500

    Renders default link

--- src/DefaultLink.js
+++ src/DefaultLink.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
+const toDString = p => 
+    `M${p[0][0]},${p[0][1]} L${p[1][0]},${p[1][1]}`
 export default class DefaultLink extends React.Component {
+    static propTypes = {
+        model: PropTypes.object
+    }
+
     render() {
-      return <text x={0} y={10}>Default Link</text>
+        const { model } = this.props
+        const { points = [[0, 0], [100, 100]] } = model
+
+        return <path stroke="#666" d={toDString(points)}/>
     }
 }
