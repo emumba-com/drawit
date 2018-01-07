7d70b60ea11fd67050c6e4934a6b6f56717c4806
umarashfaq <umar.mughal2@gmail.com>
2018-01-07 15:10:11 +0500

    Writes unit test to verify if valueBuilder updates point value correctly

---
+++ tests/utils/valueBuilder.test.js
@@ -0,0 +1,65 @@
+import expect from 'expect'
+import React from 'react'
+import {render, unmountComponentAtNode} from 'react-dom'
+
+import createValueBuilder from 'src/utils/valueBuilder'
+import sampleValue from './sampleValue'
+
+describe('utils/valueBuilder', () => {
+    const value = sampleValue
+    const onChange = () => {}
+    const conf = {}
+
+    it('exists', () => {
+        expect(createValueBuilder).toExist()
+    })
+
+    it('is a function', () => {
+        expect(createValueBuilder).toBeA('function')
+    })
+
+    it('returns a function', () => {
+        expect(createValueBuilder({
+            value,
+            onChange,
+            conf 
+        })).toBeA('function')
+    })
+
+    it('returns a function that returns an object', () => {
+        expect(createValueBuilder({
+            value,
+            onChange,
+            conf 
+        })()).toBeA('object')
+    })
+
+    it('updates a point correctly', () => {
+        let nextValue = value
+        const pointID = '61c3c50b-f2ee-45a4-8d91-74e7941ff5a7'
+        
+        // verify initial value not to be zero
+        const { x: initX, y: initY } = nextValue.points[pointID]
+        expect(initX).toNotBe(0)
+        expect(initY).toNotBe(0)
+
+        // update values with builder
+        createValueBuilder({
+            value,
+            onChange: _nextValue_ => {
+                nextValue = _nextValue_
+            },
+            conf 
+        })()
+        .updatePoint(pointID, {
+            x: 0,
+            y: 0
+        })
+        .apply()
+
+        // verify if values are correctly updated
+        const { x, y } = nextValue.points[pointID]
+        expect(x).toBe(0)
+        expect(y).toBe(0)
+    })
+})
